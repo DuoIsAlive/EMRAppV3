3e7958c6eb6ff7bbb2ca35744221bbf7c7dce1fb
Billy Softsis <48831293+DuoIsAlive@users.noreply.github.com>
2021-05-26 15:54:10 +0300

    Add project files.

---
+++ EMRAppV3/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EMRAppV3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Models.ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Models.City> Citys { get; set; }
+        public DbSet<Models.MedicalInfo> MedicalInfos { get; set; }
+        public DbSet<Models.PatientInfo> PatientInfos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ EMRAppV3/Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMRAppV3.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public bool IsDoctor { get; set; }
+
+        public ApplicationUser() { }
+
+
+
+
+    }
+}
